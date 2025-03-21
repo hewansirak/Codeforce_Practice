49beb5c2c39286b625817cc893a98ba893c29262
hewansirak <hewansg@gmail.com>
2025-03-21 23:07:07 +0300

    The best for Geleta

---
+++ B_The_best_for_Geleta.py
@@ -0,0 +1,14 @@
+# After receiving yet another integer array a1,a2,…,anat her birthday party, 
+# Geleta decides to perform some operations on it.
+
+# Formally, there are m operations that he is going to perform in order. 
+# Each of them belongs to one of the two types:
+#  + l r Given two integers l and r, for all 1≤i≤n such that l≤ai≤r, set ai:=ai+1
+#  - l r Given two integers l and r, for all 1≤i≤n such that l≤ai≤r, set ai:=ai−1
+
+# For example, if the initial array a=[7,1,3,4,3], after performing the operation + 2 4, 
+# the array a=[7,1,4,5,4]. Then, after performing the operation - 1 10, 
+# the array a=[6,0,3,4,3].
+
+# Geleta is curious about the maximum value in the array a. Please help him find it after each of the m operations.
+
