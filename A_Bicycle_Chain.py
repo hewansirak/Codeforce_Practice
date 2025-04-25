c0cc3fb8ab3dd720241ea91722481e1fa1323c89
hewansirak <hewansg@gmail.com>
2025-04-25 23:48:58 +0300

    A - Bicycle Chain

---
+++ A_Bicycle_Chain.py
@@ -0,0 +1,20 @@
+def solve():  
+    n = int(input())
+    a = list(map(int, input().split()))
+    m = int(input())
+    b = list(map(int, input().split()))
+
+    max_ratio = 0
+    count = 0
+
+    for i in range(n):
+        for j in range(m):
+            if b[j] % a[i] == 0:
+                ratio = b[j] // a[i]
+                if ratio > max_ratio:
+                    max_ratio = ratio
+                    count = 1
+                elif ratio == max_ratio:
+                    count +=1
+    print(count)
+solve()
