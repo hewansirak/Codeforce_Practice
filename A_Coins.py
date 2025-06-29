e2e8a555bf9a48772c089064a1178f223c3329ae
hewansirak <hewansg@gmail.com>
2025-06-29 23:57:11 +0300

    A - coins

---
+++ A_Coins.py
@@ -0,0 +1,16 @@
+import sys
+
+def solve():
+    n, k = map(int, sys.stdin.readline().split())
+
+    if k % 2 == 0: 
+        if n % 2 == 0: 
+            print("YES")
+        else:  
+            print("NO")
+    else:   
+        print("YES")
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    solve()
